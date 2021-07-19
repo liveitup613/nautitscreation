69ee0ecc8d5dd9f530d6c82742f0c89fb65a3280
unknown <liveitup613@outlook.com>
2021-07-19 16:17:15 +0800

    updated

--- application/views/fe/recipe.php
+++ application/views/fe/recipe.php
@@ -75,7 +75,7 @@
         </header>
         <!-- end header -->
         <!-- start page title -->
-        <section class="half-section bg-light-gray parallax" data-parallax-background-ratio="0.5" style="background-image:url('<?php echo base_url('assets/images/recipe_home_background.png');?>');">
+        <section class="padding-nine-lr bg-light-gray parallax " data-parallax-background-ratio="0.5" style="background-image:url('<?php echo base_url('assets/images/recipe_home_background.png');?>');">
             <div class="container">
                 <div class="row align-items-stretch justify-content-center extra-small-screen">
                     <div class="col-12 col-xl-6 col-lg-7 col-md-8 page-title-extra-small text-center d-flex justify-content-center flex-column">                        
@@ -94,7 +94,7 @@
                                 ?>
                             <!-- start blog item -->
                             <li class="grid-item wow animate__fadeIn">
-                                <div class="blog-post">
+                                <div class="blog-post" style='box-shadow: 0 10px 20px rgba(0,0,0,.6);'>
                                     <a href="<?php echo base_url('recipe/'.$row['ID']);?>">
                                     <div class="blog-post-image bg-gradient-fast-blue-purple" style="background-image: url('<?php echo base_url('assets/images/recipes/'.$row['Attach']);?>'); min-height: 250px; background-size: cover;">
                                     </div>                                    
