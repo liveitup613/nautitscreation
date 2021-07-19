61e4b82ffdde8abe461641b8836f57e9b254dd19
unknown <liveitup613@outlook.com>
2021-07-20 01:51:08 +0800

    updated

--- application/views/fe/media.php
+++ application/views/fe/media.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
-        <title>>Nauti T's Creation | Medai</title>
+        <title>Nauti T's Creation | Media</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="author" content="ThemeZaa">
