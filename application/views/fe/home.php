f30c64ef0f57b563427dda68f7c11a913176b83a
unknown <liveitup613@outlook.com>
2021-08-02 16:34:38 +0800

    updated

--- application/views/fe/home.php
+++ application/views/fe/home.php
@@ -737,7 +737,7 @@
                 // 'transformed = tPosition;'
             ],
         }, {
-            diffuse: 0xff007e //0000
+            diffuse: 0xd1277b //0000
         });
        
         THREE.Mesh.call(this, bufferGeometry, material);        
