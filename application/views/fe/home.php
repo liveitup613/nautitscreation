013d4aaff72871381439a27232b561b4b20c206b
unknown <liveitup613@outlook.com>
2021-08-11 16:42:27 +0800

    database update

--- application/views/fe/home.php
+++ application/views/fe/home.php
@@ -531,13 +531,13 @@
     <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/js/theme-vendors.min.js');?>"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/js/main.js');?>"></script>
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/three.js/r75/three.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r75/three.min.js'></script>
     <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/bas.js'></script>
     <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/TextGeometry.js'></script>
     <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/FontUtils.js'></script>
     <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/pnltri.min.js'></script>
     <script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/droid_sans_bold.typeface.js'></script>
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js'></script>
 
     <script>
     //capture the window loading
