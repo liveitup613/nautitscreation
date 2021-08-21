cbd0d1345c03b3d214e1337b1c590a80910d9d67
AliveSoftMan <alivesoftsnowman@gmail.com>
2021-08-21 14:26:37 +0800

    update

--- application/views/fe/home.php
+++ application/views/fe/home.php
@@ -153,7 +153,7 @@
         <div id="three-container" style='position:absolute; z-index: 1000'></div>
         <div class="position-relative" style='height: 100vh'>
             <div class="row justify-content-center">
-                <video autoplay="" loop=""
+                <video autoplay="" loop="" muted=""
                     style='max-height: 120vh; display: block; position: absolute; width: auto; height: auto; -webkit-filter: brightness(1.0); filter: brightness(1.0); '>                    
                     <source src="<?php echo base_url('assets/videos/background.mp4');?>" type="video/mp4">
                 </video>               
